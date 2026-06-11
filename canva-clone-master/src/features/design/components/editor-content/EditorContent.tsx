dd35fb8c9bf768196ece84778729ee428d38b9f2
javeed-321 <javeed@documentation.ai>
2026-06-11 18:31:54 +0530

    [200~Remove theme and styling files, including palette, theme configuration, global styles, and utility functions for downloading and thumbnail generation.~

--- canva-clone-master/src/features/design/components/editor-content/EditorContent.tsx
+++
@@ -1,6 +0,0 @@
-import { DesignFrame } from '@lidojs/design-editor';
-import { data } from '../../../../constant/data';
-
-export const EditorContent = () => {
-  return <DesignFrame data={data} />;
-};
--- canva-clone-master/src/pages/Main.tsx
+++
@@ -1,5 +0,0 @@
-import { DesignPage } from '../features/design/pages';
-
-export default function Page() {
-  return <DesignPage />;
-}
